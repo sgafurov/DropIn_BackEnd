5fc3ca20eadb26bd20d4f181dfd683be9293e977
Shakhram <shakag6@gmail.com>
2022-05-26 12:53:17 -0400

    Add env file

--- api/user.js
+++ api/user.js
@@ -69,6 +69,7 @@ router.post('/register', async (req, res) => {
         const newUser = await User.create(req.body)
         res.status(200).json(newUser)
     } catch (error) {
+        console.log(error)
         if (error.status == 400) {
             res.status(400).json(error)
         } else {
