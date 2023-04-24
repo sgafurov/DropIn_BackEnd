52835ae157702f1cbe4e8e8ee4dc46ed04b9efdf
Shakhram <shakag6@gmail.com>
2023-04-24 18:04:12 -0400

    Create basic / route

--- MERNindex.js
+++ MERNindex.js
@@ -2,7 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import userRoutes from "./MERNroutes/user.js";
 
@@ -14,24 +14,26 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
-
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
 //use express middleware to connect this to our application
 app.use("/users", userRoutes); //every route in the userRoutes will start with /users
 
 const PORT = process.env.PORT || 5000;
-const uri = process.env.ATLAS_URI
+const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
 });
 
-const connection = mongoose.connection
-connection.once('open', () => {
-  console.log("MongoDB connection established successfully")
-})
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
 
 // mongoose
 //   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
