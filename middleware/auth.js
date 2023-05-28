c8e577d5292ece9a70ed31d2483feef4e00cba07
Shakhram <shakag6@gmail.com>
2023-05-28 19:56:59 -0400

    Update jwt secret in auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ export const authRequired = (req, res, next) => {
   if (!token) {
     req.user = null; // no token and no user logged in
   } else {
-    var decoded = jwt.verify(token, "brooklyn");
+    var decoded = jwt.verify(token, process.env.JWT_SECRET);
     if (decoded) {
       // if token is correct
       req.user = decoded;
